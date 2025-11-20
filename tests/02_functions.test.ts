5aced19e2d34230be3f9997df55ee21835098af9
Andreia Negrea <andreia.negrea@endava.com>
2025-11-20 17:16:06 +0200

    Remove unnecessary tests

--- tests/02_functions.test.ts
+++ tests/02_functions.test.ts
@@ -5,8 +5,6 @@
 //   expectTypeOf(add).parameters.toEqualTypeOf<[number, number]>();
 //   expectTypeOf(add).returns.toEqualTypeOf<number>();
 //   expect(add(2, 3)).toBe(5); 
-//   add('2', '3');
-//   add(2, '3');
 // });
 
 // test('log: returns void', () => {
